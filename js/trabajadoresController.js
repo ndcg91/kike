4cb9f8b89e7d03492bd51712eda7b4918aba28bf
noel <ndcg91@yahoo.es>
2015-12-07 22:40:11 +0100

    initial

---
+++ js/trabajadoresController.js
@@ -0,0 +1,12 @@
+/**
+ * Created by noel on 07/12/2015.
+ */
+(function(){
+    var app = angular.module("Balance");
+    app.controller("TrabajadoresController",['$http','$location','$window','$timeout','$scope',function($http,$location,$window,$timeout,$scope){
+        //$window.localStorage.clear();
+        var self=this; //to access scope within callbacks
+
+
+    }]);
+})();
